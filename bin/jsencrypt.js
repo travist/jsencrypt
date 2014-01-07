64466989f02aa01ea5b1a9b30e9194e6c8dbed3d
Khatri-Bharat <khatrimonty@gmail.com>
2014-01-07 16:57:55 +0530

    RSASign and RSAVerify made compatible with OpenSSL, encryption-block formatting for private key operations was being done wrongly.

--- bin/jsencrypt.js
+++ bin/jsencrypt.js
@@ -1387,6 +1387,38 @@ function pkcs1pad2(s,n) {
   return new BigInteger(ba);
 }
 
+// PKCS#1 (type 1) pad input string s to n bytes, and return a bigint
+function pkcs1pad1(s,n) {
+  if(n < s.length + 11) { // TODO: fix for utf-8
+    console.error("Message too long for RSA");
+    return null;
+  }
+  var ba = new Array();
+  var i = s.length - 1;
+  while(i >= 0 && n > 0) {
+    var c = s.charCodeAt(i--);
+    if(c < 128) { // encode using utf-8
+      ba[--n] = c;
+    }
+    else if((c > 127) && (c < 2048)) {
+      ba[--n] = (c & 63) | 128;
+      ba[--n] = (c >> 6) | 192;
+    }
+    else {
+      ba[--n] = (c & 63) | 128;
+      ba[--n] = ((c >> 6) & 63) | 128;
+      ba[--n] = (c >> 12) | 224;
+    }
+  }
+  ba[--n] = 0;
+  while(n > 2) { 
+    ba[--n] = 0xFF;
+  }
+  ba[--n] = 1;
+  ba[--n] = 0;
+  return new BigInteger(ba);
+}
+
 // "empty" RSA key constructor
 function RSAKey() {
   this.n = null;
@@ -1447,6 +1479,35 @@ function pkcs1unpad2(d,n) {
   var i = 0;
   while(i < b.length && b[i] == 0) ++i;
   if(b.length-i != n-1 || b[i] != 2)
+    return null;
+  ++i;
+  while(b[i] != 0)
+    if(++i >= b.length) return null;
+  var ret = "";
+  while(++i < b.length) {
+    var c = b[i] & 255;
+    if(c < 128) { // utf-8 decode
+      ret += String.fromCharCode(c);
+    }
+    else if((c > 191) && (c < 224)) {
+      ret += String.fromCharCode(((c & 31) << 6) | (b[i+1] & 63));
+      ++i;
+    }
+    else {
+      ret += String.fromCharCode(((c & 15) << 12) | ((b[i+1] & 63) << 6) | (b[i+2] & 63));
+      i += 2;
+    }
+  }
+  return ret;
+}
+
+
+// Undo PKCS#1 (type 1) padding and, if valid, return the plaintext
+function pkcs1unpad2(d,n) {
+  var b = d.toByteArray();
+  var i = 0;
+  while(i < b.length && b[i] == 0) ++i;
+  if(b.length-i != n-1 || b[i] != 1)
     return null;
   ++i;
   while(b[i] != 0)
@@ -1576,7 +1637,7 @@ RSAKey.prototype.decrypt = RSADecrypt;
 // Return the PKCS#1 RSA encryption of "text" as an even-length hex string
 function RSASign(text, digestMethod) {
 	var digest = digestMethod(text); 
-	var m = pkcs1pad2(text,(this.n.bitLength()+7)>>3);
+	var m = pkcs1pad1(text,(this.n.bitLength()+7)>>3);
 	console.log("RSASign: encryption block :- " + m.toString());
 	if(m == null) return null;
 	var c = m.modPow(this.d, this.n);
@@ -1590,7 +1651,7 @@ function RSAVerify(text, signature, digestMethod) {
 	var c = parseBigInt(signature, 16);
 	var m = c.modPowInt(this.e, this.n);
 	if (m == null) return null;
-	var digest = pkcs1unpad2(m, (this.n.bitLength()+7)>>3);
+	var digest = pkcs1unpad1(m, (this.n.bitLength()+7)>>3);
 	console.log("RSAVerify: message digest :- " + digest);
 	return digest == digestMethod(text);
 }
