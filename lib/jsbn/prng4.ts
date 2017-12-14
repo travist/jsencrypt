1831462c1fbcdf78ea6bfda9928cb089c8fc83ff
Dmitry Pronin <aeoneluminaty2@gmail.com>
2017-12-14 16:51:36 +0300

    rng.ts prng4.ts

--- lib/jsbn/prng4.ts
+++ lib/jsbn/prng4.ts
@@ -1,13 +1,15 @@
 // prng4.js - uses Arcfour as a PRNG
-var Arcfour = /** @class */ (function () {
-    function Arcfour() {
+
+export class Arcfour {
+    constructor() {
         this.i = 0;
         this.j = 0;
         this.S = [];
     }
+
     //Arcfour.prototype.init = ARC4init;
     // Initialize arcfour context from key, an array of ints, each from [0..255]
-    Arcfour.prototype.init = function (key) {
+    public init(key: number[]) {
         var i, j, t;
         for (i = 0; i < 256; ++i)
             this.S[i] = i;
@@ -20,25 +22,30 @@ var Arcfour = /** @class */ (function () {
         }
         this.i = 0;
         this.j = 0;
-    };
+    }
+
     // Arcfour.prototype.next = ARC4next;
-    Arcfour.prototype.next = function () {
-        var t;
+    public next() {
+        let t;
         this.i = (this.i + 1) & 255;
         this.j = (this.j + this.S[this.i]) & 255;
         t = this.S[this.i];
         this.S[this.i] = this.S[this.j];
         this.S[this.j] = t;
         return this.S[(t + this.S[this.i]) & 255];
-    };
-    return Arcfour;
-}());
-export { Arcfour };
+    }
+
+    private i: number;
+    private j: number;
+    private S: number[];
+}
+
+
 // Plug in your RNG constructor here
 export function prng_newstate() {
     return new Arcfour();
 }
+
 // Pool size must be a multiple of 4 and greater than 32.
 // An array of bytes the size of the pool will be passed to init()
 export var rng_psize = 256;
-//# sourceMappingURL=prng4.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.js']
   },
   module: {
     rules: [
