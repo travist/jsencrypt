4d794f71c3012b137248ee9f65e141809f2a687a
Dmitry Pronin <aeoneluminaty2@gmail.com>
2017-12-14 16:09:39 +0300

    pass all tests

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+var modules_path = path.resolve(__dirname, './test');
+
+
+
+module.exports = {
+  target: 'web',
+  devtool: 'inline-source-map',
+  entry: './test/test.rsa.js',
+  output: {
+    filename: 'bundle.js',
+    chunkFilename: 'modules/[chunkhash].[name].chunk.js',
+    path: modules_path,
+    strictModuleExceptionHandling: true
+  },
+
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.tsx?$/
+      },
+    ]
+  },
+};
