24bc89786735685666df9f94d4766382912dff5d
Leandro Zuliani <lfzuliani@gmail.com>
2019-01-31 20:04:46 -0200

    Fix typeof checking of navigator object

--- lib/jsbn/jsbn.ts
+++ lib/jsbn/jsbn.ts
@@ -1849,10 +1849,11 @@ function am3(i:number, x:number, w:BigInteger, j:number, c:number, n:number) {
     return c;
 }
 
-if (j_lm && (navigator.appName == "Microsoft Internet Explorer")) {
+const inBrowser = typeof navigator !== "undefined";
+if (inBrowser && j_lm && (navigator.appName == "Microsoft Internet Explorer")) {
     BigInteger.prototype.am = am2;
     dbits = 30;
-} else if (j_lm && (navigator.appName != "Netscape")) {
+} else if (inBrowser && j_lm && (navigator.appName != "Netscape")) {
     BigInteger.prototype.am = am1;
     dbits = 26;
 } else { // Mozilla/Netscape seems to prefer am3
