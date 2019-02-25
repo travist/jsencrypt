940208a6e7dc9f56ea4ff1b41b283dae1c855158
Zixuan Liu <nodeces@gmail.com>
2019-02-25 22:42:21 +0800

    Fix export declaration

--- src/JSEncrypt.ts
+++ src/JSEncrypt.ts
@@ -20,7 +20,7 @@ export interface IJSEncryptOptions {
  * @constructor
  */
 export default class JSEncrypt {
-    constructor(options:IJSEncryptOptions) {
+    constructor(options?:IJSEncryptOptions) {
         options = options || {};
         this.default_key_size = parseInt(options.default_key_size, 10) || 1024;
         this.default_public_exponent = options.default_public_exponent || "010001"; // 65537 default openssl public exponent for rsa key type
